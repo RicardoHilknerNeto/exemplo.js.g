09b345a0505bf006e7c7cb3c477c3778cdd45271
RicardoHilknerNeto <rm550757@fiap.com.br>
2023-03-07 11:48:53 -0300

    adicionamos o script.js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//Este é um comentario de linha
+
+/**
+ * É um comentario
+ * de 
+ * Bloco
+ */
+
+//Exemplo das funções
+console.log("Olá universo")
+console.error("Bom dia!")
+console.warn("Será?")
+console.info("Vai ser...")
